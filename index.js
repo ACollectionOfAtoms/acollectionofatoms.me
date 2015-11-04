381cf6153b63cb0f736947c52d2572d3c6d868a4
acollectionofatoms <hernandezdavid88@utexas.edu>
2015-11-03 19:29:47 -0600

    Fixed Mobile layout (col-xs-6 would increase in height and skew grid layout if too much text was added) Removed opacity change on hover; reduced inset glow reworded sub-titles

--- index.js
+++ index.js
@@ -6,23 +6,20 @@ var pages = [
 ];
 
 var lastClicked;
-var lastPage;
+var lastPage; //Currently not in use!
 
 $(document).ready( function() {
     cssReset();
     $('.row').hover( 
-        function() {
+        function() { // Upon entrance
             cssReset();
             hoverExpand($(this));
             hoverShow($(this));
-            setInterval(function() {
-                $(this).toggleClass(".rowTwo")},
-                1000);
             if (lastClicked === "Git") {
-                lastClicked = "";
+                lastClicked = ""; 
                 gitCatReset();
             };
-        },
+        }, // Upon exit, just catch id.
         function() {
             lastPage = $(this).attr('id');
         }
@@ -36,19 +33,19 @@ $(document).ready( function() {
     });
 });
 
-hoverExpand = function(p) {
+hoverExpand = function(p) { // Expand div and resize others accordingly
     var page = "#" + $(p).attr("id");
     $(page).css({"height" : "40%"});
     //$(page).css({"border" : "1px solid white"});
     for (i in pages) {
         if (pages[i] != "#" + $(p).attr("id")) {
             $(pages[i]).css("height", "20%");
-            $(pages[i]).css("opacity", "0.50");
+            //$(pages[i]).css("opacity", "0.50");
         };
     };
 };
 
-hoverShow = function(p) {
+hoverShow = function(p) { // Modify div content on hover
     var page = "#" + $(p).attr("id");
     $(page + " .lead").fadeTo(200, 1.0);
     for (i in pages) {
@@ -58,7 +55,7 @@ hoverShow = function(p) {
     };
 };
 
-clickExpand = function(p) {
+clickExpand = function(p) { //Similar to hover; octocat is exception
     var page = "#" + $(p).attr("id");
     $(page).css({"height" : "76%"});
     for (i in pages) {
@@ -91,13 +88,13 @@ cssReset = function() {
     $(".title2").hide("fast");
 };
 
-gitCatReset = function() {
+gitCatReset = function() { 
     $("#Git").animate({backgroundColor: "black"});
-    // Below only works once! Need to do math...
-    $("#Git").animate({"background-position-x": "20%",
+    // More or less working properly; seems to jolt up and down (?) on mobile
+    $("#Git").animate({"background-position-x": "20%",// First get the octocat far down
                        "background-position-y": "-100%"}, 0, function() {
-        $("#Git").css({"background-image": ""});
-        $("#Git").animate({"background-position-x": "20%",
+        $("#Git").css({"background-image": ""}); // then reactivate its existence 
+        $("#Git").animate({"background-position-x": "20%",// Crawl it up to original position
                            "background-position-y": "100%"}, 1500, "linear");
     });
 };
