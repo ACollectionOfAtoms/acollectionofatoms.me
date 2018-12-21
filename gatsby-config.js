630115e6421d23547a3be495bdb63629fc366bb8
Adam <davidatomhernandez@gmail.com>
2018-12-21 15:58:04 -0500

    update readme, add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `A Collection Of Atoms`,
+    description: `A personal website of David Adam Hernandez`,
+    author: `@acollectionofatoms`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/acoa_icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hello Traveller</h1>
+    <p> This is a <a href='https://en.wikipedia.org/wiki/Website'>website</a> created by <span> David</span> <a href='https://en.wikipedia.org/wiki/Adam_(given_name)'> Adam</a> Hernandez (No one calls him <span> David</span>). </p>
+    <p>This collection of atoms obtained a B.S in <a href='https://en.wikipedia.org/wiki/Bioinformatics' target='_blank'>Biochemistry-Computation</a> from the esteemed <a href='https://www.utexas.edu/' target='_blank'>University of Texas at Austin</a>.</p>
+    <p> Nowadays he is only using the computation side of his degree as a <a href='https://en.wikipedia.org/wiki/Software_engineer'>software engineer</a> 
+      living in <a href='https://en.wikipedia.org/wiki/New_York_City'> New York City</a> (Such a lovely place!).</p>
+
+    <p>If you'd like to talk shop, feel free to <a href='#contact'>contact</a> him.</p>
   </Layout>
 )
 
