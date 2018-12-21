7b170d39e37f22780b57811bdc18a4387cacad62
Adam <davidatomhernandez@gmail.com>
2018-12-21 13:55:36 -0500

    js -> ts

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-function SEO({ description, lang, meta, keywords, title }) {
+interface SEOProps {
+  description?: string;
+  lang?: string;
+  meta?: [];
+  keywords?: string[];
+  title: string;
+}
+
+function SEO({ description, lang = `en`, meta = [], keywords = [], title }: SEOProps) {
   return (
     <StaticQuery
       query={detailsQuery}
@@ -67,19 +74,7 @@ function SEO({ description, lang, meta, keywords, title }) {
   )
 }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  keywords: [],
-}
 
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.array,
-  keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired,
-}
 
 export default SEO
 
