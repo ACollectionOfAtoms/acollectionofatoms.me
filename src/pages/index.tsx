1b53f0c65cc3adc08e35e9fd61eb472e1ac67b23
Adam <davidatomhernandez@gmail.com>
2019-07-07 15:00:53 -0400

    anchors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const IndexPage = () => (
     <img className='centeredImage' src={startrek} />
     <p> This is a <a href='https://en.wikipedia.org/wiki/Website'>website</a> created by <span> David</span> <a href='https://en.wikipedia.org/wiki/Adam_(given_name)'> Adam</a> Hernandez (No one calls him <span> David</span>). </p>
     <img className='centeredImage' src={trumpetskull} />
-    <p>This collection of atoms obtained a B.S in <a href='https://en.wikipedia.org/wiki/Bioinformatics' target='_blank'>Biochemistry-Computation</a> from the esteemed <a href='https://www.utexas.edu/' target='_blank'>University of Texas at Austin</a>.</p>
+    <p>This collection of atoms obtained a B.S in <a href='https://en.wikipedia.org/wiki/Bioinformatics'>Biochemistry-Computation</a> from the esteemed <a href='https://www.utexas.edu/'>University of Texas at Austin</a>.</p>
     <img className='centeredImage' src={creepyeye} /> 
     <p> Nowadays he is only using the computation side of his degree as a <a href='https://en.wikipedia.org/wiki/Software_engineer'>software engineer</a> living in <a href='https://en.wikipedia.org/wiki/New_York_City'> New York City</a> (Such a lovely place!).</p>
     <img className='centeredImage' src={exer} />
