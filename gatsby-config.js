91a56b3e3515574ed02bf26cd17368fd2a4f9d7b
Adam <davidatomhernandez@gmail.com>
2019-07-07 14:29:24 -0400

    update some stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,11 +19,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `A Collection Of Atoms`,
+        short_name: `acoa`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/acoa_icon.png`, // This path is relative to the root of the site.
       },
