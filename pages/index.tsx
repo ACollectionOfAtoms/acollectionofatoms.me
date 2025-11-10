8e58d9b3f0aa1c1eddd5f98a546ee5fa4180ee21
yourfriend <yourfriend.eth@outlook.com>
2025-11-10 13:08:54 -0800

    next

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      <main>
+        <h1>A Collection of Atoms</h1>
+        <h2>A website</h2>
+        <p>This is a website for a collection of atoms</p>
+      </main>
+    </div>
+  );
+}
